ccdbece213d5617098cee7db20527924b8b35209
Augusto <augustotsantos@outlook.com>
2024-04-12 00:21:21 -0300

    corrigido identação

--- src/routes/css_route.py
+++ src/routes/css_route.py
@@ -14,108 +14,98 @@ blueprint_css = Blueprint("css", __name__, url_prefix="/css")
 
 @blueprint_css.route('/categories')
 def get_categories():
-  try:
-    pipeline = [
-      {
-        "$group": {
-          "_id": {
-            "site_category_lv1": "$site_category_lv1",
-            "site_category_lv2": "$site_category_lv2"
-          },
+    try:
+        pipeline = [{
+            "$group": {
+                "_id": {
+                    "site_category_lv1": "$site_category_lv1",
+                    "site_category_lv2": "$site_category_lv2"
+                },
             "contagem": {"$sum": 1},
-        }
-      },
-      {
+            }
+        },
+        {
         "$sort": {"_id.categoria_lv1": 1}
-      }
-    ]
-    documents = mongodb.collection.aggregate(pipeline)
-    result = {"list": list(documents)}
+        }]
+        documents = mongodb.collection.aggregate(pipeline)
+        result = {"list": list(documents)}
 
-    return jsonify(result)
-  except Exception as e:
-    return jsonify({"error": str(e)}), 500
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @blueprint_css.route('/gender')
 def get_gender():
-  try:
-    pipeline = [
-      {
-        "$group": {
-          "_id": "$reviewer_gender",
-          "count": {"$sum": 1}
-        }
-      },
-      {
-        "$sort": {"count": -1}
-      }
-    ]
-    documents = mongodb.collection.aggregate(pipeline)
-    result = {"list": list(documents)}
+    try:
+        pipeline = [{
+            "$group": {
+                "_id": "$reviewer_gender",
+                "count": {"$sum": 1}
+            }
+        },
+        {
+            "$sort": {"count": -1}
+        }]
+        documents = mongodb.collection.aggregate(pipeline)
+        result = {"list": list(documents)}
 
-    return jsonify(result)
-  except Exception as e:
-    return jsonify({"error": str(e)}), 500
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @blueprint_css.route('/date')
 def get_date():
-  try:
-    pipeline = [
-      {
-        "$group": {
-          "_id": "$submission_date",
-          "count": {"$sum": 1}
-        }
-      },
-      {
-        "$sort": {"count": -1}
-      }
-    ]
-    documents = mongodb.collection.aggregate(pipeline)
-    result = {"list": list(documents)}
+    try:
+        pipeline = [{
+            "$group": {
+            "_id": "$submission_date",
+            "count": {"$sum": 1}
+            }
+        },
+        {
+            "$sort": {"count": -1}
+        }]
+        documents = mongodb.collection.aggregate(pipeline)
+        result = {"list": list(documents)}
 
-    return jsonify(result)
-  except Exception as e:
-    return jsonify({"error": str(e)}), 500
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @blueprint_css.route('/state')
 def get_state():
-  try:
-    pipeline = [
-      {
-        "$group": {
-          "_id": "$reviewer_state",
-          "count": {"$sum": 1}
-        }
-      },
-      {
-        "$sort": {"count": -1}
-      }
-    ]
-    documents = mongodb.collection.aggregate(pipeline)
-    result = {"list": list(documents)}
+    try:
+        pipeline = [{
+            "$group": {
+            "_id": "$reviewer_state",
+            "count": {"$sum": 1}
+            }
+        },
+        {
+            "$sort": {"count": -1}
+        }]
+        documents = mongodb.collection.aggregate(pipeline)
+        result = {"list": list(documents)}
 
-    return jsonify(result)
-  except Exception as e:
-    return jsonify({"error": str(e)}), 500
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @blueprint_css.route('/birth_year')
 def get_birth_year():
-  try:
-    pipeline = [
-      {
-        "$group": {
-          "_id": "$reviewer_birth_year",
-          "count": {"$sum": 1}
-        }
-      },
-      {
-        "$sort": {"count": -1}
-      }
-    ]
-    documents = mongodb.collection.aggregate(pipeline)
-    result = {"list": list(documents)}
+    try:
+        pipeline = [{
+            "$group": {
+            "_id": "$reviewer_birth_year",
+            "count": {"$sum": 1}
+            }
+        },
+        {
+            "$sort": {"count": -1}
+        }]
+        documents = mongodb.collection.aggregate(pipeline)
+        result = {"list": list(documents)}
 
-    return jsonify(result)
-  except Exception as e:
-    return jsonify({"error": str(e)}), 500
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
