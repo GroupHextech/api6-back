75fd4d0a52fb30c9f4ea6ec0dd24dabc9c2fcb7a
Augusto <augustotsantos@outlook.com>
2024-04-12 01:14:10 -0300

    add /all

--- src/routes/css_route.py
+++ src/routes/css_route.py
@@ -12,7 +12,40 @@ from ..database import mongodb
 
 blueprint_css = Blueprint("css", __name__, url_prefix="/css")
 
+@blueprint_css.route('/all')
+def get_all():
+    try:
+        pipeline = [
+            {
+                "$group": {
+                    "_id": {
+                        "submission_date": "$submission_date",
+                        "reviewer_id": "$reviewer_id",
+                        "product_id": "$product_id",
+                        "product_name": "$product_name",
+                        "product_brand": "$product_brand",
+                        "site_category_lv1": "$site_category_lv1",
+                        "site_category_lv2": "$site_category_lv2",
+                        "review_title": "$review_title",
+                        "overall_rating": "$overall_rating",
+                        "recommend_to_a_friend": "$recommend_to_a_friend",
+                        "review_text": "$review_text",
+                        "reviewer_birth_year": "$reviewer_birth_year",
+                        "reviewer_gender": "$reviewer_gender",
+                        "reviewer_state": "$reviewer_state"
+                    },
+                }
+            },
+            {
+                "$sort": {"_id.submission_date": -1}
+            }
+        ]
+        documents = client.db.css.aggregate(pipeline)
+        result = {"list": list(documents)}
 
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 
 @blueprint_css.route('/categories')
 def get_categories():
