20327aa37eaf09215a5197e974bf23b86cd2ee0b
AbraaoHenriqueVasconcelos2 <abraao.vscncls@gmail.com>
2024-04-25 21:28:30 -0300

    API6-36 :sparkles: feat: Alteramos todas as rotas da api para aceitar os parâmetros state e region

--- src/routes/css_route.py
+++ src/routes/css_route.py
@@ -50,7 +50,35 @@ def get_all():
 @blueprint_css.route('/categories')
 def get_categories():
     try:
-        pipeline = [{
+        state_params = request.args.getlist('state')
+        region_param = request.args.get('region')
+        #print (state_params)
+        filter_query = {}
+
+        
+        if region_param:
+            state_params = []
+            if region_param == 'sudeste':
+                state_params = ['SP', 'MG', 'RJ', 'ES']
+            elif region_param == 'sul':
+                state_params = ['PR', 'RS', 'SC']
+            elif region_param == 'centro-oeste':
+                state_params = ['DF', 'GO', 'MT', 'MS']
+            elif region_param == 'norte':
+                state_params = ['AC', 'AP', 'AM', 'PA', 'RO', 'RR', 'TO']
+            elif region_param == 'nordeste':
+                state_params = ['AL', 'BA', 'CE', 'MA', 'PB', 'PE', 'RN', 'SE']
+            else:
+                state_params = []
+        
+        if state_params:
+            filter_query['reviewer_state']={"$in": state_params}
+
+        pipeline = [
+         {
+            "$match": filter_query 
+        },
+        {
             "$group": {
             "_id": "$site_category_lv1",
                 "count": {"$sum": 1}  # Calculate total reviews per level 1 category
@@ -74,7 +102,35 @@ def get_categories():
 @blueprint_css.route('/gender')
 def get_gender():
     try:
-        pipeline = [{
+        state_params = request.args.getlist('state')
+        region_param = request.args.get('region')
+        #print (state_params)
+        filter_query = {}
+
+        
+        if region_param:
+            state_params = []
+            if region_param == 'sudeste':
+                state_params = ['SP', 'MG', 'RJ', 'ES']
+            elif region_param == 'sul':
+                state_params = ['PR', 'RS', 'SC']
+            elif region_param == 'centro-oeste':
+                state_params = ['DF', 'GO', 'MT', 'MS']
+            elif region_param == 'norte':
+                state_params = ['AC', 'AP', 'AM', 'PA', 'RO', 'RR', 'TO']
+            elif region_param == 'nordeste':
+                state_params = ['AL', 'BA', 'CE', 'MA', 'PB', 'PE', 'RN', 'SE']
+            else:
+                state_params = []
+        
+        if state_params:
+            filter_query['reviewer_state']={"$in": state_params}
+
+        pipeline = [
+         {
+            "$match": filter_query 
+        },
+        {
             "$group": {
                 "_id": "$reviewer_gender",
                 "count": {"$sum": 1}
@@ -93,7 +149,35 @@ def get_gender():
 @blueprint_css.route('/date')
 def get_date():
     try:
-        pipeline = [{
+        state_params = request.args.getlist('state')
+        region_param = request.args.get('region')
+        #print (state_params)
+        filter_query = {}
+
+        
+        if region_param:
+            state_params = []
+            if region_param == 'sudeste':
+                state_params = ['SP', 'MG', 'RJ', 'ES']
+            elif region_param == 'sul':
+                state_params = ['PR', 'RS', 'SC']
+            elif region_param == 'centro-oeste':
+                state_params = ['DF', 'GO', 'MT', 'MS']
+            elif region_param == 'norte':
+                state_params = ['AC', 'AP', 'AM', 'PA', 'RO', 'RR', 'TO']
+            elif region_param == 'nordeste':
+                state_params = ['AL', 'BA', 'CE', 'MA', 'PB', 'PE', 'RN', 'SE']
+            else:
+                state_params = []
+        
+        if state_params:
+            filter_query['reviewer_state']={"$in": state_params}
+            
+        pipeline = [
+         {
+            "$match": filter_query 
+        },
+        {
             "$group": {
             "_id": "$submission_date",
             "count": {"$sum": 1}
@@ -112,7 +196,35 @@ def get_date():
 @blueprint_css.route('/state')
 def get_state():
     try:
-        pipeline = [{
+        state_params = request.args.getlist('state')
+        region_param = request.args.get('region')
+        #print (state_params)
+        filter_query = {}
+
+        
+        if region_param:
+            state_params = []
+            if region_param == 'sudeste':
+                state_params = ['SP', 'MG', 'RJ', 'ES']
+            elif region_param == 'sul':
+                state_params = ['PR', 'RS', 'SC']
+            elif region_param == 'centro-oeste':
+                state_params = ['DF', 'GO', 'MT', 'MS']
+            elif region_param == 'norte':
+                state_params = ['AC', 'AP', 'AM', 'PA', 'RO', 'RR', 'TO']
+            elif region_param == 'nordeste':
+                state_params = ['AL', 'BA', 'CE', 'MA', 'PB', 'PE', 'RN', 'SE']
+            else:
+                state_params = []
+        
+        if state_params:
+            filter_query['reviewer_state']={"$in": state_params}
+
+        pipeline = [
+         {
+            "$match": filter_query 
+        },
+        {
             "$group": {
             "_id": "$reviewer_state",
             "count": {"$sum": 1}
@@ -131,7 +243,35 @@ def get_state():
 @blueprint_css.route('/birth_year')
 def get_birth_year():
     try:
-        pipeline = [{
+        state_params = request.args.getlist('state')
+        region_param = request.args.get('region')
+        #print (state_params)
+        filter_query = {}
+
+        
+        if region_param:
+            state_params = []
+            if region_param == 'sudeste':
+                state_params = ['SP', 'MG', 'RJ', 'ES']
+            elif region_param == 'sul':
+                state_params = ['PR', 'RS', 'SC']
+            elif region_param == 'centro-oeste':
+                state_params = ['DF', 'GO', 'MT', 'MS']
+            elif region_param == 'norte':
+                state_params = ['AC', 'AP', 'AM', 'PA', 'RO', 'RR', 'TO']
+            elif region_param == 'nordeste':
+                state_params = ['AL', 'BA', 'CE', 'MA', 'PB', 'PE', 'RN', 'SE']
+            else:
+                state_params = []
+        
+        if state_params:
+            filter_query['reviewer_state']={"$in": state_params}
+
+        pipeline = [
+        {
+            "$match": filter_query 
+        },
+        {
             "$group": {
             "_id": "$reviewer_birth_year",
             "count": {"$sum": 1}
