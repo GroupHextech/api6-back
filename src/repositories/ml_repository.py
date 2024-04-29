b523e76f18e0fd2b33b7348663bacf92cb35166f
Augusto <augustotsantos@outlook.com>
2024-04-28 21:25:15 -0300

    att ml e rota de acordo com feedback

--- src/repositories/ml_repository.py
+++ src/repositories/ml_repository.py
@@ -3,5 +3,5 @@ from joblib import load
 import pandas as pd
 
 def load_sentiment_model():
-    modelo_carregado, X_ngrams, df_results_true_count = load('D:\\Codigos\\Fatec\\api6\HEXTECH-API6sem\\api6-back\\machineLearning\\modelo_e_vetorizador.joblib')
-    return modelo_carregado, X_ngrams, df_results_true_count
+    model_ngrams, ngram_vectorizer, df_values, df_values_count = load('D:\\Codigos\\Fatec\\api6\HEXTECH-API6sem\\api6-back\\machineLearning\\modelo_e_vetorizador.joblib')
+    return model_ngrams, ngram_vectorizer, df_values, df_values_count
--- src/routes/ml_route.py
+++ src/routes/ml_route.py
@@ -39,24 +39,46 @@ def get_feeling():
         if state_params:
             filter_query['reviewer_state']={"$in": state_params}
 
-        modelo_carregado, X_ngrams, df_results_true_count = load_sentiment_model()
+        model_ngrams, ngram_vectorizer, df_values, df_values_count = load_sentiment_model()
 
         # Converter df_results_true_count para um dicionário
-        sentiment_counts_dict = df_results_true_count.to_dict()
+        sentiment_dict = df_values.to_dict()
 
         # Retornar o dicionário como JSON
-        return jsonify(sentiment_counts_dict)
+        return jsonify(sentiment_dict)
 
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
-@blueprint_ml.route('/feeling_test')
-def get_feeling_test():
+@blueprint_ml.route('/feeling_count')
+def get_feeling_count():
     try:
-        modelo_carregado, X_ngrams, df_results_true_count = load_sentiment_model()
+        state_params = request.args.getlist('state')
+        region_param = request.args.get('region')
+        filter_query = {}
+
+        if region_param:
+            state_params = []
+            if region_param == 'sudeste':
+                state_params = ['SP', 'MG', 'RJ', 'ES']
+            elif region_param == 'sul':
+                state_params = ['PR', 'RS', 'SC']
+            elif region_param == 'centro-oeste':
+                state_params = ['DF', 'GO', 'MT', 'MS']
+            elif region_param == 'norte':
+                state_params = ['AC', 'AP', 'AM', 'PA', 'RO', 'RR', 'TO']
+            elif region_param == 'nordeste':
+                state_params = ['AL', 'BA', 'CE', 'MA', 'PB', 'PE', 'RN', 'SE']
+            else:
+                state_params = []
+
+        if state_params:
+            filter_query['reviewer_state']={"$in": state_params}
+
+        model_ngrams, ngram_vectorizer, df_values, df_values_count = load_sentiment_model()
 
         # Converter df_results_true_count para um dicionário
-        sentiment_counts_dict = df_results_true_count.to_dict()
+        sentiment_counts_dict = df_values_count.to_dict()
 
         # Retornar o dicionário como JSON
         return jsonify(sentiment_counts_dict)
