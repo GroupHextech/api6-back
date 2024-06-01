7675ee3a7c3ad503b00ad94d7d23a8f57773275d
Michel Rubens <michelrube@gmail.com>
2024-06-01 20:22:26 -0300

    API6-137 :sparkles: feat: Reestruturação do backend para 2FA

--- src/app.py
+++ src/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 from flask_cors import CORS
 import warnings
 import os
-from .routes import blueprint_review, blueprint_blacklist, blueprint_fbusers
+from .routes import blueprint_review, blueprint_blacklist, blueprint_fbusers, bp_generate, bp_verify
 from .database import mongodb, firebase
 from dotenv import load_dotenv
 import firebase_admin
@@ -17,12 +17,15 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb+srv://admin:1234@api6.u8duoyj.mongodb.net/develop?ssl=true&retryWrites=true&w=majority&appName=Api6"
 CORS(app)
 
+# Inicia o Mongo
+mongodb.init_db(app)
+
 # Register blueprint
 app.register_blueprint(blueprint_review)
 app.register_blueprint(blueprint_blacklist)
 app.register_blueprint(blueprint_fbusers)
-# Inicia o Mongo
-mongodb.init_db(app)
+app.register_blueprint(bp_generate)
+app.register_blueprint(bp_verify)
 
 # Inicia o Firebase
 firebase.init_firestore()
