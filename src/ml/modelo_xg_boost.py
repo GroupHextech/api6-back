143311552f203e54cfe58f13f25b533e2857a32a
Augusto <augustotsantos@outlook.com>
2024-06-07 19:47:51 -0300

    API-6 44 :recycle: refactor: retirado split

--- src/ml/modelo_xg_boost.py
+++ src/ml/modelo_xg_boost.py
@@ -1,4 +1,5 @@
 import re
+import os
 import string
 import unicodedata
 import pandas as pd
@@ -7,7 +8,6 @@ import nltk
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from nltk.stem import WordNetLemmatizer
-from sklearn.model_selection import train_test_split, cross_val_score, StratifiedKFold
 from sklearn.feature_extraction.text import CountVectorizer
 from joblib import dump
 import xgboost as xgb
@@ -16,8 +16,12 @@ nltk.download('stopwords')
 nltk.download('punkt')
 nltk.download('wordnet')
 
+# Define o diretório do script como diretório de trabalho atual
+script_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_dir)
+
 # Carrega o conjunto de dados
-dataset = pd.read_csv(r"D:\Codigos\Fatec\api6\HEXTECH-API6sem\api6-back\src\ml\train.csv")
+dataset = pd.read_csv("train.csv")
 # Remove linhas com valores em branco
 dataset = dataset.dropna(subset=['review_text', 'overall_rating'])
 # Cria uma nova coluna para classificar entre comentários positivos(2), negativos(0) ou neutros(1) com base na nota:
@@ -70,20 +74,17 @@ ngram_vectorizer = CountVectorizer(ngram_range=(1, 3))
 # Transforma os textos em vetores numéricos, representando a frequência de ocorrência de cada palavra e combinação de palavras
 X_ngrams = ngram_vectorizer.fit_transform(X)
 
-# Divida os dados em conjuntos de treino e teste
-X_train, X_test, Y_train, Y_test = train_test_split(X_ngrams, Y, test_size=0.2, random_state=42, stratify=Y)
-
 # Cria uma instância do modelo XGBoost
 xgboost = xgb.XGBClassifier()
 
 # Calcula os pesos de amostra com base nas classes
-class_weights = np.zeros(len(Y_train))
-class_counts = np.bincount(Y_train)
+class_weights = np.zeros(len(Y))
+class_counts = np.bincount(Y)
 for i in range(len(class_counts)):
-    class_weights[Y_train == i] = len(Y_train) / class_counts[i]
+    class_weights[Y == i] = len(Y) / class_counts[i]
 
 # Treina o modelo XGBoost com pesos de amostra
-xgboost.fit(X_train, Y_train, sample_weight=class_weights)
+xgboost.fit(X_ngrams, Y, sample_weight=class_weights)
 
 # Salva o modelo e o vetorizador em um único arquivo chamado 'modelo_xg_boost.joblib'
 dump((xgboost, ngram_vectorizer), 'modelo_xg_boost.joblib')
